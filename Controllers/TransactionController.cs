626c269d8c758a3187ca838c6f60de7c937dbae3
Dilara Benli <tesdilara2@gmail.com>
2024-11-27 09:44:29 +0300

    made asynchronous

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -1,10 +1,7 @@
 ﻿using VB_api.Data;
 using VB_api.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Client;
-using System.Security.Principal;
 
 namespace VB_api.Controllers
 {
@@ -146,74 +143,6 @@ namespace VB_api.Controllers
                     TransactionExplanation = request.Explanation
                 };
 
-                _dbContext.Transaction.Add(transactionEntity);
-                _dbContext.SaveChanges();
-
-                var transfer = new Transfer
-                {
-                    TransactionID = transactionEntity.TransactionID,
-                    SourceAccountID = request.SourceAccountID,
-                    TargetAccountID = request.TargetAccountID,
-                };
-
-                _dbContext.Transfer.Add(transfer);
-                _dbContext.SaveChanges();
-
-                transaction.Commit();
-
-                return Ok(new {message = "Transfer successful.", transferID = transfer.TransferID, 
-                    fromAccountNewBalance = sourceAccount.AccountBalance, toAccountNewBalance = targetAccount.AccountBalance});
-            }
-            catch
-            {
-                transaction.Rollback();
-                return StatusCode(500, new { message = "An error occurred during the transfer." });
-            }
-        }
-
-        /*
-        [HttpPost]
-        public async Task<IActionResult> TransferMoney1([FromBody] TransferRequest request)
-        {
-            // Transaction başlat
-            using var transaction = await _dbContext.Database.BeginTransactionAsync();
-
-            try
-            {
-                if (request.SourceAccountID == request.TargetAccountID)
-                {
-                    return BadRequest(new { message = "Source and target accounts cannot be the same." });
-                }
-
-                var sourceAccount = await _dbContext.Account.FindAsync(request.SourceAccountID);
-                if (sourceAccount == null)
-                {
-                    return BadRequest("No source account found.");
-                }
-
-                var targetAccount = await _dbContext.Account.FindAsync(request.TargetAccountID);
-                if (targetAccount == null)
-                {
-                    return BadRequest("No target account found.");
-                }
-
-                if (sourceAccount.AccountBalance < request.Amount)
-                {
-                    return BadRequest("There is not enough balance in the source account.");
-                }
-
-                sourceAccount.AccountBalance -= request.Amount;
-                targetAccount.AccountBalance += request.Amount;
-
-                var transactionEntity = new Transaction
-                {
-                    AccountID = request.SourceAccountID,
-                    TransactionTypeName = "Transfer",
-                    TransactionAmount = request.Amount,
-                    TransactionDate = DateTime.Now,
-                    TransactionExplanation = request.Explanation
-                };
-
                 await _dbContext.Transaction.AddAsync(transactionEntity);
                 await _dbContext.SaveChangesAsync();
 
@@ -227,7 +156,6 @@ namespace VB_api.Controllers
                 await _dbContext.Transfer.AddAsync(transfer);
                 await _dbContext.SaveChangesAsync();
 
-                // Transaction commit
                 await transaction.CommitAsync();
 
                 return Ok(new { transactionEntity, transfer });
@@ -238,6 +166,5 @@ namespace VB_api.Controllers
                 return StatusCode(500, $"An error occurred during the operation: {ex.Message}");
             }
         }
-        */
     }
 }
